df4b55d65872fa479220ba546d9e967ffcff5dbc
ig.nichiporuk <ig.nichiporuk@gmail.com>
2018-06-07 10:26:20 +0300

    basket 07.06.2018 change1

--- basket/js/basket.js
+++ basket/js/basket.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     
-    $(".modal-close-click").on("click", function(){
+    $(".js-close-modal").on("click", function(){
         $("body").removeClass("modal-open");
         $("body").removeClass("overlay-open");
         $(".modal").removeClass("open");
