7310862edd8c30a3dae63de1ebc576ca0ad0f793
ig.nichiporuk <ig.nichiporuk@gmail.com>
2018-06-11 11:15:02 +0300

    change class tirecart to cartitem

--- choose-item/js/choose-item.js
+++ choose-item/js/choose-item.js
@@ -1,12 +1,12 @@
  $(document).ready(function () {
     var win = $(this);
-    $(".tire-click-for-hover").on("click", function(){
+    $(".item-click-for-hover").on("click", function(){
         if (win.width() <= 1199) {
             $(this).parents().addClass("hover-open");
-            $(this).find(".tirecart-hover-body").css({
+            $(this).find(".cartitem-hover-body").css({
                 "display" : "block"
             });
-            $(this).find(".tirecart-hover-body").addClass("open");
+            $(this).find(".cartitem-hover-body").addClass("open");
             $(".allClose").addClass("open");
         };
     });
@@ -21,53 +21,53 @@
         };
     });
     $(".allClose").on("click", function(){
-        $('body').find(".tirecart-hover-body").css({
+        $('body').find(".cartitem-hover-body").css({
             "display" : "none"
         });
-        $('body').find(".tirecart-hover-body").removeClass("open");
+        $('body').find(".cartitem-hover-body").removeClass("open");
         $(".allClose").removeClass("open");
         $('html').find(".hover-open").removeClass("hover-open");
         $('html').removeClass("hover-open");
     });
-    $(".tirecart-hover-body_close").on('click', function(){
+    $(".cartitem-hover-body_close").on('click', function(){
         $(this).parent().css({
             "display" : "none"
         });
-        $('body').find(".tirecart-hover-body").removeClass("open");
+        $('body').find(".cartitem-hover-body").removeClass("open");
         $(".allClose").removeClass("open");
         $('html').find(".hover-open").removeClass("hover-open");
         $('html').removeClass("hover-open");
     });
     $(window).on('resize', function(){
         if (win.width() >= 1200) {
-            $('body').find(".tire-click-for-hover").removeClass("tire-click-for-hover");
+            $('body').find(".item-click-for-hover").removeClass("item-click-for-hover");
             $('body').find(".click-for-hover-cross").removeClass("click-for-hover-cross");
-            $('body').find(".tirecart-hover-body").removeAttr("style");
-            $('body').find(".tirecart-hover-body").removeClass("open");
+            $('body').find(".cartitem-hover-body").removeAttr("style");
+            $('body').find(".cartitem-hover-body").removeClass("open");
             $(".allClose").removeClass("open");
             $('html').find("div.hover-open").removeClass("hover-open");
             $('html').removeClass("hover-open");
         }
         else{
-            $('body').find(".tirecart-hover-body").parent().addClass("tire-click-for-hover")
+            $('body').find(".cartitem-hover-body").parent().addClass("item-click-for-hover")
         }
     });
    
     $(document).on('scroll', function(){
         var scroll_top = $(document).scrollTop();
         var scroll_bottom = $(window).scrollTop() + $(window).height();
-        var tirecart_height = $(".tirecart").outerHeight();
-        var tirecart_offset = $(".tirecart.hover-open").offset().top;
-        var scroll_vallue_top = tirecart_offset + 50;
-        var scroll_vallue_bottom = tirecart_offset;
+        var cartitem_height = $(".cartitem").outerHeight();
+        var cartitem_offset = $(".cartitem.hover-open").offset().top;
+        var scroll_vallue_top = cartitem_offset + 50;
+        var scroll_vallue_bottom = cartitem_offset;
         if (win.width() <= 1199) {
-            if((Number(scroll_top) > Number(scroll_vallue_top)) || (Number(scroll_bottom) < Number(tirecart_offset))){
-                $('body').find(".tirecart-hover-body.open").css({
+            if((Number(scroll_top) > Number(scroll_vallue_top)) || (Number(scroll_bottom) < Number(cartitem_offset))){
+                $('body').find(".cartitem-hover-body.open").css({
                     "display" : "none"
                 });
                 $('html').find("div.hover-open").removeClass("hover-open");
                 $('html').removeClass("hover-open");
-                $('body').find(".tirecart-hover-body.open").removeClass("open");
+                $('body').find(".cartitem-hover-body.open").removeClass("open");
                 $(".allClose").removeClass("open")
             }
         };
